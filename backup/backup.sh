d3f983f445ef1c8de3b8dd7fb1912e74855577a6
pingu <284400329+halfoctane@users.noreply.github.com>
2026-05-14 05:32:33 +0000

    initial commit

---
+++ backup/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+until mariadb -h database_host -u root -p"$MYSQL_ROOT_PASSWORD" --skip-ssl -e "SELECT 1;" > /dev/null 2>&1; do
+  echo "Database not ready yet... retrying in 3 seconds"
+  sleep 3
+done
+
+echo "Starting Zero-Touch Archive process..."
+
+BACKUP_FILE="/archives/backup_$(date +%F_%H-%M-%S).sql"
+
+mariadb-dump -h database_host -u root -p"$MYSQL_ROOT_PASSWORD" --all-databases --skip-ssl > $BACKUP_FILE
+
+echo "Backup securely saved to $BACKUP_FILE"
+
+sleep 86400
