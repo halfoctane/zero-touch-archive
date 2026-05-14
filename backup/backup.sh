7be3ede66d496a5bb5ce7d5d4f83900b0488b56a
pingu <284400329+halfoctane@users.noreply.github.com>
2026-05-14 05:47:34 +0000

    Add: Compress backups to .gz

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,10 +7,13 @@ done
 
 echo "Starting Zero-Touch Archive process..."
 
-BACKUP_FILE="/archives/backup_$(date +%F_%H-%M-%S).sql"
+BACKUP_FILE="/archives/backup_$(date +%F_%H-%M-%S).sql.gz"
 
 mariadb-dump -h database_host -u root -p"$MYSQL_ROOT_PASSWORD" --all-databases --skip-ssl > $BACKUP_FILE
 
 echo "Backup securely saved to $BACKUP_FILE"
+
+find /archives -name "*.sql.gz" -mtime +7 -delete
+echo "Old backups cleaned up."
 
 sleep 86400
